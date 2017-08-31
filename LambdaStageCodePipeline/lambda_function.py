3ab0fb1b955f622cfa1c15bc55942e531db13aae
Joy Prakash Sharma <droidlabour@gmail.com>
2017-08-31 12:06:36 +0530

    Copy LambdaStage code from internal repo to make it generic

--- LambdaStageCodePipeline/lambda_function.py
+++ LambdaStageCodePipeline/lambda_function.py
@@ -6,21 +6,25 @@ import zipfile
 from os.path import basename
 from botocore.client import Config
 
-cp = boto3.client('codepipeline')
+codepipeline = boto3.client('codepipeline')
 s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
 cfn = boto3.client('cloudformation')
 
 
 def get_templt_body(s):
-    return json.dumps(cfn.get_template(StackName=s)['TemplateBody'])
+    b = cfn.get_template(StackName=s)['TemplateBody']
+    if isinstance(b, str):
+        return b
+    else:
+        return json.dumps(b)
 
 
 def get_templt_param(s):
-    x = cfn.describe_stacks(StackName=s)['Stacks'][0]['Parameters']
+    stack_params = cfn.describe_stacks(StackName=s)['Stacks'][0]['Parameters']
     p = dict()
     p['Parameters'] = dict()
-    for i in x:
-        if 'ECSTaskDesiredCount' == i['ParameterKey']:
+    for i in stack_params:
+        if i['ParameterKey'] == 'ECSTaskDesiredCount' and i['ParameterValue'] == '0':
             p['Parameters'][i['ParameterKey']] = os.getenv('ECSTaskDesiredCount')
         else:
             p['Parameters'][i['ParameterKey']] = i['ParameterValue']
@@ -32,21 +36,24 @@ def handler(event, context):
     job_id = event['CodePipeline.job']['id']
     output_artifact_key = event['CodePipeline.job']['data']['outputArtifacts'][0]['location']['s3Location']['objectKey']
     output_artifact_bucket = event['CodePipeline.job']['data']['outputArtifacts'][0]['location']['s3Location']['bucketName']
-    data = dict()
-    templt_file = '/tmp/' + 'ecs_app.json'
-    param_file = '/tmp/' + 'ecs_app_param.json'
-    data[templt_file] = get_templt_body(os.getenv('AppStackName'))
-    data[param_file] = get_templt_param(os.getenv('AppStackName'))
+    artifact_files = {}
+    artificat_pkg_file_name = '/tmp/templts.zip'
+    artifact_files['/tmp/ecs_app.json'] = get_templt_body(os.getenv('ECSAPPStackName'))
+    artifact_files['/tmp/ecs_app_param.json'] = get_templt_param(os.getenv('ECSAPPStackName'))
+
     try:
-        os.remove('/tmp/templts.zip')
+        print 'Removing ' + artificat_pkg_file_name
+        os.remove(artificat_pkg_file_name)
     except OSError:
         pass
-    for i in data:
+
+    for i in artifact_files:
         with open(i, 'w') as f:
-            f.write(data[i])
-        with zipfile.ZipFile('/tmp/templts.zip', 'a') as z:
+            print 'Writing ' + i
+            f.write(artifact_files[i])
+        with zipfile.ZipFile(artificat_pkg_file_name, 'a') as z:
             z.write(i, basename(i))
-    x = open('/tmp/templts.zip', 'rb')
+    x = open(artificat_pkg_file_name, 'rb')
     s3.put_object(Bucket=output_artifact_bucket, Key=output_artifact_key, Body=x, ServerSideEncryption='aws:kms', ContentType='application/zip')
-    cp.put_job_success_result(jobId=job_id)
+    codepipeline.put_job_success_result(jobId=job_id)
     return 0
